84f5862910d44457a4413b98d4c057591869ba37
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-04 20:10:18 +0100

    Add search bar to movies app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SearchBar from './components/MovieSearch'
 
 function App() {
 
   return (
-    <h1>Movies App</h1>
-
-
+    <>
+      <h1>Movies App</h1>
+      <SearchBar />
+    </>
   )
 }
 
---
+++ src/components/MovieSearch.jsx
@@ -0,0 +1,11 @@
+function SearchBar() {
+    return (
+        <div>
+            <i className="fas fa-search"></i>
+            <input type="text" placeholder="Search for a movie..." />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar;
