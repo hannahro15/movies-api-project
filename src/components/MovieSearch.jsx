ecd02a5455ee91d18e5acb2f02f54c73686a57d2
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-05 23:14:51 +0100

    Finish implementing the search functionality

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,9 +1,22 @@
-function SearchBar() {
+import { useState } from 'react';
+
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearch] = useState('');
+
+    function handleSearch() {
+        onSearch(searchTerm);
+    }
+
     return (
         <div>
             <i className="fas fa-search"></i>
-            <input type="text" placeholder="Search for a movie..." />
-            <button>Search</button>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for a movie..."
+            />
+            <button onClick={handleSearch}>Search</button>
         </div>
     )
 }
