7f0003c3d5c234d1a3c900b356e295d4e91806dd
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-05 16:27:39 +0100

    Started to set up search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,34 @@ import MovieCard from './components/MovieCard'
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState(false);
 
-  async function fetchMovies() {
+  async function searchMovies(search) {
+    if (!search) return;
+
+    setSearch(true);
     try {
-      const response = await fetch(`https://www.omdbapi.com/?s=movie&type=movie&apikey=${import.meta.env.VITE_OMDB_API_KEY}`);
+      const response = await fetch(`https://www.omdbapi.com/?s=${search}&type=movie&apikey=${import.meta.env.VITE_OMDB_API_KEY}`);
       const result = await response.json();
-      console.log(result);
-      if (result.Search) {
-        setMovies(result.Search);
-      }
+      setMovies(result.Search || []);
     } catch (error) {
       console.error("Error fetching movies");
+      setMovies([]);
     }
   }
-
-  useEffect(() => {
-    fetchMovies();
-  }, []);
 
   return (
     <>
       <h1>Movies App</h1>
-      <SearchBar />
+      <SearchBar onSearch={searchMovies} />
       <div className="movie-card-container d-flex flex-wrap justify-content-center">
-        {movies.map((movie) => (
-          <MovieCard key={movie.imdbID} movie={movie} />
-        ))}
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))
+        ) : (
+          <p>{search ? "No movies found. Try a different search!" : "Search for movies to get started!"}</p>
+        )}
       </div>
     </>
   )
