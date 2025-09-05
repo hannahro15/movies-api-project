cee040bbcf9906604927c51b25b02b00223b2a3e
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-05 16:07:28 +0100

    Fetch data from movies api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 import SearchBar from './components/MovieSearch'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMovies() {
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?s=popular&apikey=${import.meta.env.VITE_OMDB_API_KEY}`);
+      const result = await response.json();
+      console.log(result);
+      if (result.Search) {
+        setMovies(result.Search);
+      }
+    } catch (error) {
+      console.error("Error fetching movies");
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <>
       <h1>Movies App</h1>
       <SearchBar />
       <div className="movie-card-container">
-        <MovieCard />
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </>
   )
